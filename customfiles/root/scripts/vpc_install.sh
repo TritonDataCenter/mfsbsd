265e56934b926b5245ec5fe6043cbfa6d8057e63
Sean Chittenden <sean@chittenden.org>
2018-03-21 18:13:39 -0700

    lang/go is already installed and available in the image.

--- customfiles/root/scripts/vpc_install.sh
+++ customfiles/root/scripts/vpc_install.sh
@@ -10,6 +10,5 @@
 set -e
 set -u
 
-/usr/local/sbin/pkg install -y lang/go || true
 /usr/local/bin/go get github.com/joyent/freebsd-vpc/cmd/vpc
 /usr/bin/install -h -g wheel -o root -m 0755 $(/usr/local/bin/go env GOPATH)/bin/vpc /usr/local/bin
